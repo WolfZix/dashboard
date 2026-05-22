b6d7ad4c64e53fe1b82d3175aacbd14490206494
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-05-22 21:38:04 +0200

    fixing lightMode 90% done

--- app/components/navbar/ProfileButton/EditProfileModal.tsx
+++ app/components/navbar/ProfileButton/EditProfileModal.tsx
@@ -16,7 +16,7 @@ export default function EditProfileModal({
   const [newUsername, setNewUsername] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [color, setColor] = useState(user.color);
-  const [bio, setBio] = useState(user.bio);
+  const [bio, setBio] = useState("");
   const [avatar, setAvatar] = useState(user.avatar || "");
   const [banner, setBanner] = useState(user.banner || "");
   let isActive = false;
@@ -115,12 +115,12 @@ export default function EditProfileModal({
 
   function getRoleColor(role: string) {
     return role === "Admin"
-      ? "bg-red-500"
+      ? "bg-red-500 text-white"
       : role === "Moderator"
-        ? "bg-orange-500"
+        ? "bg-orange-500 text-white"
         : role === "Premium"
-          ? "bg-purple-500"
-          : "bg-slate-500";
+          ? "bg-purple-500 text-white"
+          : "bg-slate-500 text-white";
   }
 
   const colors = [
@@ -138,36 +138,39 @@ export default function EditProfileModal({
   ];
 
   return (
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-999 p-6 overflow-y-auto">
-      <div className="w-140 max-w-full rounded-[28px] bg-slate-900 border border-slate-800 overflow-hidden shadow-2xl my-auto">
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-999 p-6 overflow-y-auto transition-all duration-300">
+      <div className="w-140 max-w-full rounded-[28px] bg-slate-900 light:bg-slate-200 border border-slate-800 overflow-hidden shadow-2xl my-auto transition-all duration-300">
         {/* Header */}
         <div
           style={{
             borderColor: `${color}50`,
             boxShadow: `0 0 20px ${color}30`,
           }}
-          className="relative h-44 bg-linear-to-br from-slate-950 to-slate-800 border-b overflow-hidden transition-all duration-300"
+          className="relative h-44 bg-linear-to-br from-slate-950 to-slate-800 light:bg-slate-300 border-b overflow-hidden transition-all duration-300"
         >
           {banner ? (
             <img
               src={banner}
-              className="absolute inset-0 w-full h-full object-cover"
+              className="absolute inset-0 w-full h-full object-cover transition-all duration-300"
             />
           ) : null}
 
           {/* Overlay */}
-          <div className="absolute inset-0 bg-black/85"></div>
+          <div
+            className={`absolute inset-0 bg-black/85 transition-all duration-300
+          ${color === "#ffffff" ? "light:bg-black/50" : ""}`}
+          ></div>
 
           {/* Close button */}
           <button
             onClick={() => onClose()}
-            className="z-20 absolute top-5 right-5 w-10 h-10 rounded-xl bg-slate-950/70 hover:bg-slate-900 border border-slate-700 backdrop-blur-md flex items-center justify-center transition cursor-pointer"
+            className="z-20 absolute top-5 right-5 w-10 h-10 rounded-xl bg-slate-950/70 light:bg-white hover:bg-slate-900 border light:hover:bg-slate-200 border-slate-700 light:border-slate-400 backdrop-blur-md flex items-center justify-center transition-all duration-300 cursor-pointer"
           >
             <X size={20} />
           </button>
 
           {/* Banner edit */}
-          <label className="absolute bottom-5 right-5 z-20 w-11 h-11 rounded-2xl bg-slate-950/70 hover:bg-slate-900 border border-slate-700 backdrop-blur-md flex items-center justify-center transition cursor-pointer">
+          <label className="absolute bottom-5 right-5 z-20 w-11 h-11 rounded-2xl light:text-slate-950 bg-slate-950/70 light:bg-white hover:bg-slate-900 border light:hover:bg-slate-200 border-slate-700 light:border-slate-400 backdrop-blur-md flex items-center justify-center transition-all duration-300 cursor-pointer">
             ✎
             <input
               type="file"
@@ -178,7 +181,7 @@ export default function EditProfileModal({
           </label>
 
           {/* Profile */}
-          <div className="absolute left-8 bottom-6 z-20 flex items-end gap-5">
+          <div className="absolute left-8 bottom-6 z-20 flex items-end gap-5 transition-all duration-300">
             {/* Avatar */}
             <div
               style={{
@@ -190,12 +193,12 @@ export default function EditProfileModal({
             >
               <div
                 style={{ backgroundColor: user.color }}
-                className="w-28 h-28 rounded-full flex items-center justify-center text-black text-5xl font-bold select-none shadow-2xl m-1.25"
+                className="w-28 h-28 rounded-full flex items-center justify-center text-black text-5xl font-bold select-none shadow-2xl m-1.25 transition-all duration-300"
               >
                 {avatar ? (
                   <img
                     src={avatar}
-                    className="w-full h-full object-cover rounded-full scale-[1.01]"
+                    className="w-full h-full object-cover rounded-full scale-[1.01] transition-all duration-300"
                   />
                 ) : (
                   user.name[0].toUpperCase()
@@ -203,7 +206,7 @@ export default function EditProfileModal({
               </div>
 
               {/* Avatar edit */}
-              <label className="absolute -bottom-1 -right-1 w-9 h-9 rounded-full bg-slate-900 border border-slate-700 flex items-center justify-center hover:bg-slate-800 transition cursor-pointer">
+              <label className="absolute -bottom-1 -right-1 w-9 h-9 rounded-full bg-slate-900 border border-slate-700 light:bg-white light:border-slate-400 flex items-center justify-center hover:bg-slate-800 light:hover:bg-slate-200 transition-all duration-300 cursor-pointer">
                 ✎
                 <input
                   type="file"
@@ -215,77 +218,97 @@ export default function EditProfileModal({
             </div>
 
             {/* User info */}
-            <div className="pb-2">
-              <div className="flex items-center gap-3 mb-2">
-                <div className="text-4xl font-bold">{user.name}</div>
+            <div className="pb-2 transition-all duration-300">
+              <div className="flex items-center gap-3 mb-2 transition-all duration-300">
+                <div
+                  style={{
+                    color: `${color}`,
+                  }}
+                  className="text-4xl font-bold transition-all duration-300"
+                >
+                  {user.name}
+                </div>
 
                 <div
-                  className={`px-4 py-1 rounded-full ${getRoleColor(user.role)} text-sm font-semibold`}
+                  className={`px-4 py-1 rounded-full ${getRoleColor(user.role)} text-sm font-semibold transition-all duration-300`}
                 >
                   {user.role}
                 </div>
               </div>
 
-              <p className="text-sm text-slate-300">Joined: {user.joined}</p>
+              <p className="text-sm text-slate-300 transition-all duration-300">
+                Joined: {user.joined}
+              </p>
             </div>
           </div>
         </div>
 
         {/* Content */}
-        <div className="p-8 flex flex-col gap-6">
+        <div className="p-8 flex flex-col gap-6 transition-all duration-300">
           {/* Username */}
           <div>
-            <p className="text-sm text-slate-400 mb-2">Change Username</p>
+            <p className="text-sm text-slate-400 light:text-slate-700 mb-2 transition-all duration-300">
+              Change Username
+            </p>
 
             <input
               type="text"
               value={newUsername}
               onChange={(e) => setNewUsername(e.target.value)}
               placeholder={user.name}
-              className="w-full rounded-2xl bg-slate-950 border border-slate-800 px-4 py-4 outline-none focus:border-slate-700 transition"
+              className="w-full rounded-2xl bg-slate-950 border border-slate-800 light:bg-white light:border-slate-300 light:focus:border-slate-400 px-4 py-4 outline-none focus:border-slate-700 transition-all duration-300"
             />
           </div>
 
           {/* Password */}
           <div>
-            <p className="text-sm text-slate-400 mb-2">Change Password</p>
+            <p className="text-sm text-slate-400 light:text-slate-700 mb-2 transition-all duration-300">
+              Change Password
+            </p>
 
             <input
               type="password"
               value={newPassword}
+              placeholder="•••••••••"
               onChange={(e) => setNewPassword(e.target.value)}
-              className="w-full rounded-2xl bg-slate-950 border border-slate-800 px-4 py-4 outline-none focus:border-slate-700 transition"
+              className="w-full rounded-2xl bg-slate-950 border border-slate-800 light:bg-white light:border-slate-300 light:focus:border-slate-400 px-4 py-4 outline-none focus:border-slate-700 transition-all duration-300"
             />
           </div>
 
           {/* Bio */}
           <div>
-            <p className="text-sm text-slate-400 mb-2">Bio</p>
+            <p className="text-sm text-slate-400 light:text-slate-700 mb-2">
+              Bio
+            </p>
 
             <textarea
               value={bio}
               onChange={(e) => setBio(e.target.value)}
-              placeholder="Tell something about yourself..."
-              className="w-full h-15 resize-none rounded-2xl bg-slate-950 border border-slate-800 p-4 outline-none focus:border-slate-700 transition"
+              placeholder={user.bio}
+              className="w-full h-15 resize-none rounded-2xl bg-slate-950 border border-slate-800 light:bg-white light:border-slate-300 light:focus:border-slate-400 p-4 outline-none focus:border-slate-700 transition-all duration-300"
             ></textarea>
           </div>
 
           {/* Profile color */}
           <div>
-            <p className="text-sm text-slate-400 mb-3">Profile Color</p>
-            <div className="flex flex-wrap gap-3 justify-center">
+            <p className="text-sm text-slate-400 light:text-slate-700 mb-3 transition-all duration-300">
+              Profile Color
+            </p>
+            <div className="flex flex-wrap gap-3 justify-center transition-all duration-300">
               {colors.map((buttonColor) => (
                 <button
                   key={buttonColor}
                   onClick={() => setColor(buttonColor)}
                   style={{ "--bg": `${buttonColor}` } as React.CSSProperties}
-                  className={`w-7 h-7 rounded-full bg-(--bg) border-2 hover:scale-105 transition cursor-pointer
+                  className={`w-7 h-7 rounded-full bg-(--bg) border-2 hover:scale-105 transition-all duration-300 cursor-pointer
                     ${
                       color === buttonColor
                         ? buttonColor === "#ffffff"
                           ? "shadow-[inset_0_0_0_2px_rgb(0,0,0)] scale-110"
-                          : "border-white scale-110"
-                        : "border-transparent"
+                          : buttonColor === "#000000"
+                            ? "shadow-[inset_0_0_0_2px_rgb(255,255,255)] scale-110"
+                            : "border-white light:border-black scale-110"
+                        : "border-transparent light:border-slate-300"
                     }
                     `}
                 ></button>
@@ -294,10 +317,10 @@ export default function EditProfileModal({
           </div>
 
           {/* Buttons */}
-          <div className="flex gap-3 pt-2">
+          <div className="flex gap-3 pt-2 transition-all duration-300">
             <button
               onClick={onClose}
-              className="flex-1 py-4 rounded-2xl bg-slate-950 hover:bg-slate-800 border border-slate-700 transition cursor-pointer"
+              className="flex-1 py-4 rounded-2xl bg-slate-950 hover:bg-slate-800 light:bg-white light:border-slate-300 light:hover:bg-slate-200 border border-slate-700 transition-all duration-300 cursor-pointer"
             >
               Cancel
             </button>
@@ -306,16 +329,22 @@ export default function EditProfileModal({
               onClick={saveChanges}
               style={
                 {
-                  borderColor: `${color}90`,
                   boxShadow: `0px 0px 20px ${color}50`,
+                  "--border-color": `${color}90`,
                   "--text": `${color}`,
                   "--hover-text": `${color}`,
                   "--bg": `${color}20`,
                   "--hover-bg": `${color}70`,
                 } as React.CSSProperties
               }
-              className={`flex-1 py-4 border rounded-2xl font-semibold cursor-pointer transition-all duration-300 hover:scale-[1.02] active:scale-[0.98] hover:bg-(--hover-bg) bg-(--bg) text-(--text) hover:text-(--hover-text)
-                ${color === "#000000" ? "text-white/75 bg-black/75 hover:text-white hover:bg-black" : ""}`}
+              className={`flex-1 py-4 border border-(--border-color) rounded-2xl font-semibold cursor-pointer transition-all duration-300 hover:scale-[1.02] active:scale-[0.98] hover:bg-(--hover-bg) bg-(--bg) text-(--text) hover:text-(--hover-text)
+                ${
+                  color === "#000000"
+                    ? "text-white/75 bg-black/75 hover:text-white hover:bg-black"
+                    : color === "#ffffff"
+                      ? "light:text-black/75 light:bg-white light:border-slate-300 light:hover:text-black light:hover:bg-slate-200"
+                      : ""
+                }`}
             >
               Save Changes
             </button>
