22032e7666b3d15b78b5dd33b9f8d15784d45c3a
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-05-30 23:59:44 +0200

    daily news fixed

--- app/components/OverviewPage/DailyNews/DailyNews.tsx
+++ app/components/OverviewPage/DailyNews/DailyNews.tsx
@@ -9,7 +9,6 @@ type DailyNewsProps = {
 
 export default function DailyNews({ news }: DailyNewsProps) {
   const marqueeRef = useRef<HTMLDivElement>(null);
-  const [marqueeWidth, setMarqueeWidth] = useState(0);
   const [page, setPage] = useState(0);
   const [flashDirection, setFlashDirection] = useState<"left" | "right" | null>(
     null,
@@ -17,31 +16,22 @@ export default function DailyNews({ news }: DailyNewsProps) {
   const [canAnimate, setCanAnimate] = useState(
     localStorage.getItem("animations") === "true",
   );
-
-  useEffect(() => {
-    if (marqueeRef.current) {
-      setMarqueeWidth(marqueeRef.current.scrollWidth / 2);
-    }
-  }, [news]);
-
+  const mode = localStorage.getItem("mode");
+  const isCompact = mode === "compact";
+  const isComfortNoAnimations = mode === "comfortable" && !canAnimate;
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    if (!canAnimate || localStorage.getItem("mode") !== "compact") {
-      return;
-    }
-
+    if (!canAnimate || isCompact) return;
     intervalRef.current = setInterval(() => {
       setPage((p) => (p + 1) % news.length);
-      setFlashDirection("right");
     }, 5000);
-
     return () => {
       if (intervalRef.current) {
         clearInterval(intervalRef.current);
       }
     };
-  }, [news.length, canAnimate]);
+  }, [news.length, canAnimate, isCompact]);
 
   useEffect(() => {
     if (!flashDirection) return;
@@ -80,10 +70,9 @@ export default function DailyNews({ news }: DailyNewsProps) {
 
   return (
     <>
-      {localStorage.getItem("mode") !== "compact" && (
-        <AnimatePresence>
-          <div
-            className={`
+      {!isCompact && !isComfortNoAnimations && (
+        <div
+          className={`
               bg-slate-900
               border border-slate-700
               rounded-2xl
@@ -106,9 +95,9 @@ export default function DailyNews({ news }: DailyNewsProps) {
               duration-300
               ${canAnimate ? "overflow-hidden" : "overflow-x-auto"}
             `}
-          >
-            <h2
-              className="
+        >
+          <h2
+            className="
                 font-semibold
                 text-2xl
                 select-none
@@ -120,121 +109,282 @@ export default function DailyNews({ news }: DailyNewsProps) {
                 transition-all
                 duration-300
               "
-            >
-              <DigitalClock />
-            </h2>
-
-            <div
-              className={`
-                border-y-2
-                border-slate-700
-                light:border-[#e2e8f0]
-                mb-2
-                compact:mb-1
-                transition-all
-                duration-300
-                noAnimations:transition-none
-                ${canAnimate ? "overflow-hidden" : "overflow-x-auto"}
-              `}
-            >
+          >
+            <DigitalClock />
+          </h2>
+          <div
+            className="
+            border-y-2
+            border-slate-700
+            light:border-[#e2e8f0]
+            mb-2
+            overflow-hidden
+          "
+          >
+            <AnimatePresence mode="wait">
               <motion.div
-                ref={marqueeRef}
-                className="
-                  flex
-                  w-max
-                  gap-3
-                  compact:gap-1.5
-                  will-change-transform
-                "
-                animate={
-                  canAnimate
-                    ? {
-                        x: [0, -marqueeWidth],
-                      }
-                    : { x: 0 }
-                }
+                key={page}
+                initial={canAnimate ? { opacity: 0, x: 10 } : false}
+                animate={canAnimate ? { opacity: 1, x: 0 } : undefined}
+                exit={canAnimate ? { opacity: 0, x: -10 } : undefined}
                 transition={
                   canAnimate
                     ? {
-                        ease: "linear",
-                        duration: 350,
-                        repeat: Infinity,
-                        repeatType: "loop",
+                        duration: 0.2,
+                        ease: "easeInOut",
                       }
                     : undefined
                 }
+                className="
+                min-h-18
+                px-5
+                py-3
+                text-base
+                text-center
+                text-slate-300
+                light:text-[#475569]
+                select-none
+              "
               >
-                {[...news, ...news].map((message, index) => (
-                  <p
-                    key={index}
-                    className="
-                        text-sm
-                        whitespace-nowrap
-                        min-w-fit
-                        pl-2
-                        pr-3
-                        py-5
-                        compact:pl-1
-                        compact:pr-1.5
-                        compact:py-2.5
-                        text-slate-300
-                        light:text-[#475569]
-                        transition-all
-                        duration-300
-                      "
-                  >
-                    {message}
-                  </p>
-                ))}
+                {news[page]}
               </motion.div>
-            </div>
+            </AnimatePresence>
           </div>
-        </AnimatePresence>
+          <div className="flex justify-center gap-1.5">
+            {Array.from({ length: news.length }).map((_, i) => (
+              <div
+                key={i}
+                onClick={() => {
+                  setPage(i);
+                  resetInterval();
+                }}
+                className={`
+                h-2.5
+                w-7
+                cursor-pointer
+                rounded-full
+                transition-all
+                duration-300
+                ${
+                  i === page
+                    ? "bg-[#22c55e]"
+                    : "bg-slate-700 light:bg-[#cbd5e1] light:hover:bg-[#b2bbc6]"
+                }
+              `}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+
+      {isComfortNoAnimations && (
+        <div
+          className={`
+              bg-slate-900
+              border border-slate-700
+              rounded-2xl
+              col-span-full
+              min-h-35
+              mt-2
+              py-3
+              compact:mt-0
+              compact:py-1.5
+              compact:min-h-[100%]
+              compact:col-span-1
+              compact:hidden
+              light:bg-[white]
+              light:border-[#e2e8f0]
+              light:hover:bg-[#f8fafc]
+              light:shadow-[0_1px_3px_rgba(0,0,0,0.1)]
+              scrollbar-thumb-(--scrollbar-thumb)
+              light:scrollbar-thumb-(--scrollbar-thumb)
+              transition-all
+              duration-300
+              ${canAnimate ? "overflow-hidden" : "overflow-x-auto"}
+            `}
+        >
+          <h2
+            className="
+                font-semibold
+                text-2xl
+                select-none
+                mx-5
+                mb-2
+                compact:mx-2.5
+                compact:mb-1
+                compact:text-xl
+                transition-all
+                duration-300
+              "
+          >
+            <DigitalClock />
+          </h2>
+          <div
+            className="
+            relative
+            border-y-2
+            border-slate-700
+            light:border-[#e2e8f0]
+            mb-2
+            overflow-hidden
+            flex
+            justify-center
+          "
+          >
+            <button
+              onClick={() =>
+                setPage((prev) => (prev === 0 ? news.length - 1 : prev - 1))
+              }
+              className="
+              absolute
+              left-0
+              top-0
+              bottom-0
+              z-10
+              w-10
+              flex
+              items-center
+              justify-center
+              bg-slate-900/30
+              light:bg-white/30
+              hover:bg-slate-800/60
+              light:hover:bg-slate-100/70
+              transition-all
+              duration-300
+              cursor-pointer
+              text-slate-400
+              hover:text-lime-400
+            "
+            >
+              <ArrowLeft size={24} />
+            </button>
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={page}
+                initial={canAnimate ? { opacity: 0, x: 10 } : false}
+                animate={canAnimate ? { opacity: 1, x: 0 } : undefined}
+                exit={canAnimate ? { opacity: 0, x: -10 } : undefined}
+                transition={
+                  canAnimate
+                    ? {
+                        duration: 0.2,
+                        ease: "easeInOut",
+                      }
+                    : undefined
+                }
+                className="
+                min-h-18
+                max-w-275
+                px-5
+                py-3
+                text-base
+                text-center
+                text-slate-300
+                light:text-[#475569]
+                select-none
+              "
+              >
+                {news[page]}
+              </motion.div>
+            </AnimatePresence>
+            <button
+              onClick={() =>
+                setPage((prev) => (prev === news.length - 1 ? 0 : prev + 1))
+              }
+              className="
+              absolute
+              right-0
+              top-0
+              bottom-0
+              z-10
+              w-10
+              flex
+              items-center
+              justify-center
+              bg-slate-900/30
+              light:bg-white/30
+              hover:bg-slate-800/60
+              light:hover:bg-slate-100/70
+              transition-all
+              duration-300
+              cursor-pointer
+              text-slate-400
+              hover:text-lime-400
+            "
+            >
+              <ArrowRight size={24} />
+            </button>
+          </div>
+          <div className="flex justify-center gap-1.5">
+            {Array.from({ length: news.length }).map((_, i) => (
+              <div
+                key={i}
+                onClick={() => {
+                  setPage(i);
+                  resetInterval();
+                }}
+                className={`
+                h-2.5
+                w-7
+                cursor-pointer
+                rounded-full
+                transition-all
+                duration-300
+                ${
+                  i === page
+                    ? "bg-[#22c55e]"
+                    : "bg-slate-700 light:bg-[#cbd5e1] light:hover:bg-[#b2bbc6]"
+                }
+              `}
+              />
+            ))}
+          </div>
+        </div>
       )}
 
       {localStorage.getItem("mode") === "compact" && (
         <div
           className={`
-            bg-slate-900
-            border border-slate-700
-            rounded-2xl
-            mt-0
-            py-1.5
-            min-h-full
-            col-span-1
-            overflow-hidden
-            light:bg-[white]
-            light:border-[#e2e8f0]
-            light:hover:bg-[#f8fafc]
-            light:shadow-[0_1px_3px_rgba(0,0,0,0.1)]
-            transition-all
-            duration-300
-            flex
-            flex-col
+        bg-slate-900
+          border border-slate-700
+          rounded-2xl
+          mt-0
+          py-1.5
+          min-h-full
+          col-span-1
+          overflow-hidden
+          light:bg-[white]
+          light:border-[#e2e8f0]
+          light:hover:bg-[#f8fafc]
+          light:shadow-[0_1px_3px_rgba(0,0,0,0.1)]
+          transition-all
+          duration-300
+          flex
+          flex-col
           `}
         >
           <h2
             className="
-              font-semibold
-              select-none
-              mx-2.5
-              mb-2
-              text-xl
-              transition-all
-              duration-300
+            font-semibold
+            select-none
+            mx-2.5
+            mb-2
+            text-xl
+            transition-all
+            duration-300
             "
           >
             <DigitalClock />
           </h2>
           <div
             className="
-              flex-1
-              overflow-hidden
-              border-y-2
-              border-slate-700
-              light:border-[#e2e8f0]
-              transition-all
-              duration-300
+            flex-1
+            overflow-hidden
+            border-y-2
+            border-slate-700
+            light:border-[#e2e8f0]
+            transition-all
+            duration-300
             "
           >
             <AnimatePresence mode="wait">
@@ -273,16 +423,16 @@ export default function DailyNews({ news }: DailyNewsProps) {
                     : undefined
                 }
                 className="
-                  flex
-                  h-full
-                  px-4
-                  py-4
-                  compact:px-2
-                  compact:py-2
-                  text-base
-                  text-slate-300
-                  light:text-[#475569]
-                  select-none
+                flex
+                h-full
+                px-4
+                py-4
+                compact:px-2
+                compact:py-2
+                text-base
+                text-slate-300
+                light:text-[#475569]
+                select-none
                 "
               >
                 {news[page]}
@@ -291,12 +441,12 @@ export default function DailyNews({ news }: DailyNewsProps) {
           </div>
           <div
             className="
-              mt-3
-              flex
-              items-center
-              justify-around
-              px-4
-              compact:px-2
+            mt-3
+            flex
+            items-center
+            justify-around
+            px-4
+            compact:px-2
             "
           >
             <button
@@ -305,30 +455,30 @@ export default function DailyNews({ news }: DailyNewsProps) {
                 resetInterval();
               }}
               className={`
-                w-8
-                h-8
-                flex
-                items-center
-                justify-center
-                rounded-full
-                bg-slate-800
-                hover:bg-slate-700
-                light:bg-slate-200
-                light:hover:bg-slate-300
-                transition-all
-                duration-300
-                cursor-pointer
-                hover:text-lime-500
+              w-8
+              h-8
+              flex
+              items-center
+              justify-center
+              rounded-full
+              bg-slate-800
+              hover:bg-slate-700
+              light:bg-slate-200
+              light:hover:bg-slate-300
+              transition-all
+              duration-300
+              cursor-pointer
+              hover:text-lime-500
               `}
             >
               <ArrowLeft size={16} />
             </button>
             <span
               className="
-                text-sm
-                text-lime-400
-                light:text-[#475569]
-                select-none
+              text-sm
+              text-lime-400
+              light:text-[#475569]
+              select-none
               "
             >
               {page + 1} / {news.length}
@@ -339,22 +489,22 @@ export default function DailyNews({ news }: DailyNewsProps) {
                 resetInterval();
               }}
               className={`
-                w-8
-                h-8
-                flex
-                items-center
-                justify-center
-                rounded-full
-                bg-slate-800
-                hover:bg-slate-700
-                light:bg-slate-200
-                light:hover:bg-slate-300
-                transition-all
-                duration-300
-                cursor-pointer
-                hover:text-lime-500
-                ${flashDirection === "right" ? "text-lime-500" : ""}
-              `}
+              w-8
+              h-8
+              flex
+              items-center
+              justify-center
+              rounded-full
+              bg-slate-800
+              hover:bg-slate-700
+              light:bg-slate-200
+              light:hover:bg-slate-300
+              transition-all
+              duration-300
+              cursor-pointer
+              hover:text-lime-500
+              ${flashDirection === "right" ? "text-lime-500" : ""}
+            `}
             >
               <ArrowRight size={16} />
             </button>
